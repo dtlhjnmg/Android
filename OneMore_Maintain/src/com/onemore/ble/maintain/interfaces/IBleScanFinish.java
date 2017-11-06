afe0b381026dbd096f9e875fb39bd4783f8f35c6
jay <dtlhjnmg@163.com>
2017-11-06 17:57:58 +0800

    test github

--- OneMore_Maintain/src/com/onemore/ble/maintain/interfaces/IBleScanFinish.java
+++
@@ -1,9 +0,0 @@
-package com.onemore.ble.maintain.interfaces;
-
-/**
- * @author wuzhiyi
- * @date 2015-4-18 ����9:08:32
- */
-public interface IBleScanFinish {	
-	public void onBleScanFinished();
-}
--- OneMore_Maintain/src/com/onemore/ble/maintain/services/IMessageResponseCallBack.java
+++
@@ -1,7 +0,0 @@
-package com.onemore.ble.maintain.services;
-
-import com.onemore.ble.maintain.bean.DeviceInfo;
-
-public interface IMessageResponseCallBack {
-    public void onDataResponse(DeviceInfo lightInfo);
-}
